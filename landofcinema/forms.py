e933ee9f4dd4b7664c48ea925a54ef2b73e90dad
naingolan <kelvinsdechaw@gmail.com>
2023-01-19 10:35:48 +0300

    The first one today

--- landofcinema/forms.py
+++ landofcinema/forms.py
@@ -1,3 +1,8 @@
 from django import forms
-
-class BookSeatForm(forms.Form):
+class BookingForm(forms.Form):
+    movie_title = forms.CharField(max_length=255)
+    theater_name = forms.CharField(max_length=255)
+    show_date = forms.DateField()
+    no_of_seats = forms.IntegerField()
+    email = forms.EmailField()
+    phone_number = forms.CharField(max_length=255)
--- landofcinema/urls.py
+++ landofcinema/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+
+from landofcinema.forms import BookingForm
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+]
+
+from .views import book_seats
+
+urlpatterns = [
+    path('book/<int:movie_id>/', BookingForm, name='booking_form'),
 ]
