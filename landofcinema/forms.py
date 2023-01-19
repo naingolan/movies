c9dfbf4945fe2dce63333963e319cc9781efcb21
naingolan <kelvinsdechaw@gmail.com>
2023-01-19 18:38:08 +0300

    the new one fuck old

--- landofcinema/forms.py
+++ landofcinema/forms.py
@@ -5,3 +5,4 @@ class BookingForm(ModelForm):
     class Meta:
         model = Booking
         fields = ['user','movie','theater','screen','seat_number','booking_date']
+        #i have deciede to addthis i have lost my data fuck  
