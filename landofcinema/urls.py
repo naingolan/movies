8ed967e61f8237e4a20397669f929c507e9a1aa7
naingolan <kelvinsdechaw@gmail.com>
2023-01-20 23:45:25 +0300

    This is the last last for today

--- landofcinema/urls.py
+++ landofcinema/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from landofcinema.forms import BookingForm, ScheduleForm
 from . import views
-from .views import ScheduleListView
+from .views import ScheduleListView, confirm_payment
 
 
 urlpatterns = [
@@ -12,5 +12,6 @@ urlpatterns += [
     path('book_seats/', views.book_seats, name='booking_form'),
     path('add_schedule/', views.schedule_create, name='schedule'),
     path('schedules/', ScheduleListView.as_view(), name='schedule_list'),
+    path('confirm_payment/', views.confirm_payment, name='confirm_payment'),
 
 ]
